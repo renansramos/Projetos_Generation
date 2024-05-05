c2a5e12df46b63ba5179f5cde594c59b05e54317
root <renan_dasilvaramos@hotmail.com>
2024-05-05 17:43:21 -0300

    Implementando as Classes ContaCorrente e ContaPoupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,6 +3,8 @@ package conta;
 import java.util.Scanner;
 import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
@@ -15,7 +17,25 @@ public class Menu {
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
+		
+		//Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(1200.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
 
+		//Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		
+		
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
